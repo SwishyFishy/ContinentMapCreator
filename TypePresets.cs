9b74eb8380ee962cbfa9fe6ec65dde250e4d8632
Jonah Galloway-Fenwick <jonahcoyotegf@icloud.com>
2024-06-13 17:39:51 -0400

    Separate Form methods into partial class in new file and add partial class file for setting presets

---
+++ TypePresets.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace ContinentMapCreator
+{
+    public partial class form_Window : Form
+    {
+        private void LoadDefaults()
+        {
+
+        }
+    }
+}
